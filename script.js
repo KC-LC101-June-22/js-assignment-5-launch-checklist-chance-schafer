a841c6c4b54be13b66d304a63231af8e8275b519
Chance Schafer <chance.schafer.dev@gmail.com>
2022-08-22 13:44:13 -0500

    completed script.js

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 // Write your JavaScript code here!
 
+
 window.addEventListener("load", function() {
 
+    let form = document.querySelector('form');
+    
+    form.addEventListener('submit', function(event) {
+        let pilot = document.getElementById('pilot').value;
+        let copilot = document.getElementById('copilot').value;
+        let fuelLevel = document.getElementById('fuelLevel').value;
+        let cargoLevel = document.getElementById('cargoMass').value;
+
+        formSubmission(document, event, pilot, copilot, fuelLevel, cargoLevel);
+
+    }) 
+
+    myFetch();
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
-       console.log(listedPlanets);
    }).then(function () {
-       console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+       let planet = pickPlanet(listedPlanets);
+       addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image)
    })
    
 });
+
+// window.addEventListener("load", function() {
+//     let form = document.querySelector('form');
+    
+//     form.addEventListener('submit', function(event) {
+//         let pilot = document.getElementById('pilot').value;
+//         let copilot = document.getElementById('copilot').value;
+//         let fuelLevel = document.getElementById('fuelLevel').value;
+//         let cargoLevel = document.getElementById('cargoMass').value;
+
+//         formSubmission(document, event, pilot, copilot, fuelLevel, cargoLevel);
+//     })
+// })
